a68aec811d4f5eff6c5b0932749c4dd41d5eb0d9
Deekshith Allamaneni <dkh.hyd@gmail.com>
2016-04-10 11:19:15 -0500

    Add Boilerplate manifest, ContentScript

---
+++ src/js/content/main.js
@@ -0,0 +1,11 @@
+/**
+ * @file
+ * Content Script Provides Input Form On HN Website
+ * Allowing To Filter Posts By Votes and
+ * Directly Navigate to Given Page Number
+ *
+ * @author Deekshith Allamaneni
+ * @copyright 2016 Docs Online Viewer
+ */
+"use strict";
+
