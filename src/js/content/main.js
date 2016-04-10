8a036ab0d017d8e3f92b3036925d5e523d7ad36b
Deekshith Allamaneni <dkh.hyd@gmail.com>
2016-04-10 12:52:15 -0500

    Add Page Input Field Submit Btn. Change Min and Max Values

--- src/js/content/main.js
+++ src/js/content/main.js
@@ -15,7 +15,9 @@ hnPageTopElement[0].innerHTML = hnPageTopElement[0].innerHTML
         `
         |
         <form action="/over" style='display:inline; margin: 0; padding: 0;'>
-            <input placeholder="votes" type="number" name="points" style="display: inline;" maxlength="4" size="4" min="0" max="9999">
+            votes <input placeholder="votes" type="number" name="points" style="display: inline;" maxlength="4" size="4" min="1" max="9999" value="1">
+            page <input placeholder="page" type="number" name="p" style="display: inline;" min="1" max="99" value="1">
+            <input type="submit" value="Go!" style="display: inline;">
         </form>
         `
     );
